69e171e5a4b387e6781234cbfa89ec4b52ce2879
Steve Tran <stran@redhat.com>
2023-08-21 14:19:53 -0400

    Add logic for fileprocessing

---
+++ src/main/java/io/steve/Driver.java
@@ -0,0 +1,112 @@
+package io.steve;
+
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+
+import io.fabric8.kubernetes.api.model.batch.v1.Job;
+import io.fabric8.kubernetes.api.model.batch.v1.JobBuilder;
+import io.fabric8.kubernetes.client.KubernetesClientBuilder;
+import io.fabric8.openshift.client.OpenShiftClient;
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+@QuarkusMain
+public class Driver {
+
+    private static final String FILE_PATH = "/var/tmp/";
+
+    public static void main(String ... args) {
+        System.out.println("Running main method");
+
+        Quarkus.run(MyApp.class);
+    }
+
+
+    public static class MyApp implements QuarkusApplication {
+
+        @Override
+        public int run(String... args) throws Exception {
+            Path path = Paths.get(FILE_PATH);
+            WatchService watcher = FileSystems.getDefault().newWatchService();
+            path.register(watcher, StandardWatchEventKinds.ENTRY_CREATE);
+
+
+            try {
+                while (true) {
+                    // Get directory changes:
+                    // take() is a blocking method that waits for a signal from the monitor before returning.
+                    // You can also use the watcher.poll() method, a non-blocking method that will immediately return
+                    // whether there is a signal in the watcher at that time.
+                    // The returned result, WatchKey, is a singleton object,
+                    // which is the same as the instance returned by the previous register method.
+                    WatchKey key = watcher.take();
+                    // Handling file change events：
+                    // key.pollEvents()It is used to obtain file change events,
+                    // which can only be obtained once and cannot be obtained repeatedly,
+                    // similar to the form of a queue.
+                    for (WatchEvent<?> event : key.pollEvents()) {
+                        // event.kind()：event type
+                        if (event.kind() == StandardWatchEventKinds.OVERFLOW) {
+                            //event may be lost or discarded
+                            continue;
+                        }
+                        // Returns the path (relative path) of the file or directory that triggered the event
+                        Path fileName = (Path) event.context();
+
+
+                        if(!fileName.endsWith(".processing") && !fileName.endsWith(".done")){
+                            System.out.println("file changed: " + fileName);
+
+
+
+                           // TODO create k8s job
+
+                            try (OpenShiftClient client = new KubernetesClientBuilder().build().adapt(OpenShiftClient.class)) {
+                                Job job = new JobBuilder()
+                                        .withNewMetadata()
+                                            .withName("file-processor-" + fileName.getFileName())
+                                        .endMetadata()
+                                        .withNewSpec()
+                                            .withNewTemplate()
+                                                .withNewSpec()
+                                                    .addNewContainer()
+                                                        .withName("sleep-container")
+                                                        .withImage("alpine")
+                                                        .withCommand("sleep", "30")
+                                                    .endContainer()
+                                                    .withRestartPolicy("Never")
+                                                .endSpec()
+                                            .endTemplate()
+                                        .endSpec()
+                                        .build();
+
+                                client.batch().v1().jobs().inNamespace(client.getNamespace()).resource(job).create();
+
+                            } catch (Exception e) {
+                                System.err.println("Error creating Kubernetes Job: " + e.getMessage());
+                               e.printStackTrace();
+                            }
+
+                        }
+                    }
+                    // This method needs to be reset every time the take() or poll() method of WatchService is called
+                    if (!key.reset()) {
+                        break;
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            Quarkus.waitForExit();
+            return 0;
+        }
+    }
+
+}
